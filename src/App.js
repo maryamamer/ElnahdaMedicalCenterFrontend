b7fd359c218b18019faf8bb41965162419c3aa54
Nada494 <nada.taha9411@gmail.com>
2022-02-27 00:08:41 +0200

    responsive

--- src/App.js
+++ src/App.js
@@ -39,25 +39,7 @@ import Chatbot from './components/chatbot/Chatbot'
 
 
 export default function App (props) {
-  const isMobileDevice = useMediaQuery({
-    query: "(min-device-width: 480px)",
-  });
-
-  const isTabletDevice = useMediaQuery({
-    query: "(min-device-width: 768px)",
-  });
-
-  const isLaptop = useMediaQuery({
-    query: "(min-device-width: 1024px)",
-  });
-
-  const isDesktop = useMediaQuery({
-    query: "(min-device-width: 1200px)",
-  });
-
-  const isBigScreen = useMediaQuery({
-    query: "(min-device-width: 1201px )",
-  });
+  
     return (
       
       <div className="App">
