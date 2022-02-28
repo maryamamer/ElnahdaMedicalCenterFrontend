88145484f6d270d1425330c03c6a1cc56ae5e8b8
maryamamer <maryamamer931@gmail.com>
2022-02-28 02:59:55 +0200

    Payment-System

--- src/components/Dr_profile.js
+++ src/components/Dr_profile.js
@@ -81,6 +81,11 @@ function Dr_Profile({ isregistered, remAppointment }, props) {
       {/* <li className='nav-item'>
             <Link className='nav-link' to='/signup'>Sign Up</Link>
         </li> */}
+        <Link to={`/Payment/${doctor.id}`}>
+        <button type="button" className="btns">
+          ادفع الكشف
+        </button>
+      </Link>
     </Fragment>
   );
 
