3f0737b2c42db73932610e237bce2b73abbc5a63
Maram Khaled <52356509+MaramRaslan45@users.noreply.github.com>
2022-03-05 13:43:41 +0200

    admin (#49)

--- src/pages/adduser.js
+++ src/pages/adduser.js
@@ -11,26 +11,20 @@ function Adduser({ doctor,isdoctor }) {
     dispatch(get_doctor());
   }, []);
   const [userinfo, setuserinfo] = useState({
-    password: "",
-    last_login: "",
-    is_superuser: "",
-    first_name: "",
-    last_name: "",
-    is_staff: "",
-    is_active: "",
-    date_joined: "",
     username: "",
-    fullname: "",
     email: "",
-    phone: "",
+    password: "",
+    fullname: "",
+    image: '',
     date_of_birth: "",
-    address: "",
     age: "",
-    image: null,
+    address: "",
+    phone: "",
+    doctors:[],
     gender: "",
     guardian_number: "",
     guardian_relation: "",
-    doctor_id: "",
+
   });
 
   console.log(userinfo);
@@ -47,26 +41,23 @@ function Adduser({ doctor,isdoctor }) {
       setuserinfo({ ...userinfo, [e.target.name]: false });
     }
   };
+  const add_doctors = (e)=>{
+    const id=parseInt(e.target.value)
+    setuserinfo({...userinfo,doctors:[...doctors,{id:id}]})
+  }
   const [repassword,setrepassword]=useState('')
   const handleimage = (e) => {
     let img = e.target.files[0];
     setuserinfo({ ...userinfo, image: img });
   };
-  const handlefile = (e) => {
-    let file = e.target.files[0];
-    setuserinfo({ ...userinfo, report: file });
-  };
+
   const {
     password,
-    last_login,
-    is_superuser,
-    is_staff,
-    is_active,
-    date_joined,
     username,
     fullname,
     email,
     phone,
+    doctors,
     date_of_birth,
     address,
     age,
@@ -74,12 +65,10 @@ function Adduser({ doctor,isdoctor }) {
     gender,
     guardian_number,
     guardian_relation,
-    doctor_id,
   } = userinfo;
 
   const adduser = (e) => {
     e.preventDefault();
-
     const fd = new FormData();
     fd.append("username", username);
     fd.append("email", email);
@@ -87,19 +76,13 @@ function Adduser({ doctor,isdoctor }) {
     fd.append("fullname", fullname);
     fd.append("image", image);
     fd.append("date_of_birth", date_of_birth);
-    fd.append("is_superuser", is_superuser);
-    fd.append("is_active", is_active);
-    fd.append("last_login", last_login);
-    fd.append("date_joined", date_joined);
     fd.append("age", age);
     fd.append("address", address);
-    fd.append("is_staff", is_staff);
     fd.append("phone", phone);
-    fd.append("doctor_id", doctor_id);
+    fd.append("doctors", doctors);
     fd.append("gender", gender);
     fd.append("guardian_number", guardian_number);
     fd.append("guardian_relation", guardian_relation);
- 
     dispatch(add_user(fd,username,password,email,repassword));
   };
 
@@ -107,7 +90,7 @@ function Adduser({ doctor,isdoctor }) {
   return (
     <>
       <form
-        method="post"
+      
         className=" bg-light m-3"
         noValidate
         onSubmit={(e) => adduser(e)}
@@ -137,7 +120,7 @@ function Adduser({ doctor,isdoctor }) {
               onChange={(e) => onChange(e)}
               required
             />
-            <label htmlFor="repassword">اعادة الرقم السري</label>
+            {/* <label htmlFor="repassword">اعادة الرقم السري</label>
             <input
               type="text"
               className="form-control"
@@ -147,7 +130,7 @@ function Adduser({ doctor,isdoctor }) {
               onChange={(e)=>{setrepassword(e.target.value)}}
               required
             />
-          <div className="valid-feedback">Looks good!</div>
+          <div className="valid-feedback">Looks good!</div> */}
           </div>
           <div className="col-md-4 mb-3">
             <label htmlFor="email">ميل المستخدم</label>
@@ -261,65 +244,9 @@ function Adduser({ doctor,isdoctor }) {
             <div className="valid-feedback">Looks good!</div>
           </div>
         </div>
-        <div className="form-row">
-          <div className="col-md-6 mb-3">
-            <label htmlFor="last_login">تاريخ أخر زيارة</label>
-            <input
-              type="date"
-              className="form-control"
-              id="last_login"
-              name="last_login"
-              value={last_login}
-              onChange={(e) => onChange(e)}
-            />
-            <div className="valid-feedback">Looks good!</div>
-          </div>
-          <div className="col-md-6 mb-3">
-            <div className="custom-control custom-checkbox mb-3">
-              <input
-                type="checkbox"
-                className="custom-control-input"
-                value={is_staff}
-                id="is_staff"
-                name="is_staff"
-                onChange={(e) => handlechange(e)}
-              />
-              <label htmlFor="is_staff" className="custom-control-label">
-                من العاملين بالمركز ؟
-              </label>
-              <div className="valid-feedback">Looks good!</div>
-            </div>
-          </div>
-        </div>
+      
 
-        <div className="form-row">
-          <div className="custom-control custom-checkbox mb-3">
-            <input
-              type="checkbox"
-              className="custom-control-input"
-              id="is_active"
-              name="is_active"
-              value={is_active}
-              onChange={(e) => handlechange(e)}
-            />
-            <label htmlFor="is_active" className="custom-control-label">
-              هل مازال متواصل معنا؟
-            </label>
-            <div className="valid-feedback">Looks good!</div>
-          </div>
-          <div className="col-md-6 mb-3">
-            <label htmlFor="date_joined">تاريخ تسجيل الدخول</label>
-            <input
-              type="date"
-              className="form-control"
-              value={date_joined}
-              id="date_joined"
-              name="date_joined"
-              onChange={(e) => onChange(e)}
-            />
-            <div className="valid-feedback">Looks good!</div>
-          </div>
-        </div>
+       
         <div className="form-row">
           <div className="col-md-6 mb-3">
             <label htmlFor="guardian_number">بطاقة الواصي ان وجد</label>
@@ -348,30 +275,20 @@ function Adduser({ doctor,isdoctor }) {
             <div className="valid-feedback">Looks good!</div>
           </div>
         </div>
-        <div className="custom-control custom-checkbox mb-3">
-          <input
-            type="checkbox"
-            className="custom-control-input"
-            id="is_superuser"
-            value={is_superuser}
-            name="is_superuser"
-            onChange={(e) => handlechange(e)}
-          />
-          <label className="custom-control-label" for="is_superuser">
-            ادمن؟
-          </label>
-        </div>
+    
         <div className="form-group">
-          <label htmlFor="doctor_id">الطبيب الخاص</label>
+          <label htmlFor="doctors">الطبيب الخاص</label>
           <select
-            className="custom-select "
-            onChange={(e) => onChange(e)}
-            name="doctor_id"
-            value={doctor_id}
-            id="doctor_id"
+            multiple={true}
+            className="custom-select"
+            onChange={(e) => add_doctors(e)}
+            name="doctors"
+            // value={userinfo.doctor_ids}
+            id="doctors"
+            required
           >
             {doctor.map((d) => {
-              return <option value={d.id}>{d.id}</option>;
+              return <option key={d.id} value={d.id}>{d.fullname}</option>;
             })}
           </select>
         </div>
